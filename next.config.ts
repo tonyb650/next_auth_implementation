98d37c5c2adcd6c1a5763ae8eb28e460c07b6eed
Tony Brierly <anthonylbrierly@gmail.com>
2025-09-15 15:30:12 -0700

    Another update to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        '@prisma/client': '@prisma/client',
+      })
+    }
+    return config
   },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client']
+  }
 };
 
 export default nextConfig;
