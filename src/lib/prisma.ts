8330d97a23fcb2ac255f6f2a11162c488460e376
Tony Brierly <anthonylbrierly@gmail.com>
2025-09-15 16:39:43 -0700

    Checking environment variable once more again

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,7 +6,7 @@ const globalForPrisma = global as unknown as {
 
 const prisma = globalForPrisma.prisma || new PrismaClient()
 console.log("Creating Prisma")
-console.log("process.env.NODE_ENV !== 'production' = "+ process.env.NODE_ENV !== 'production')
+console.log("process.env.NODE_ENV !== 'production' = "+ (process.env.NODE_ENV !== 'production'))
 console.log("Done")
 
 //if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
